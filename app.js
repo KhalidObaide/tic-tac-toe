6a2d31e135ec35bea59039837741c68eab1b6dad
Khalid Obaide <sayedkhalidobaide@gmail.com>
2021-08-17 21:40:46 +0430

    Initialize the project, Apply Main Concepts

---
+++ app.js
@@ -0,0 +1,141 @@
+const APP = document.querySelector("#app");
+
+/* Global Variables */
+const player1 = 'X';
+const player2 = 'O'; // CPU by default
+let turn = player1;
+let originalBoard;
+let gameover = false;
+const isBotPlaying = true;
+let cellsDOM = [];
+
+function displayModal(text){
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+
+    if(text == "TIE"){
+        modal.innerText = "It's a DRAW!";
+    }else if(text == player1){
+        modal.innerText = "It's Impossible, YOU WON";
+    }else if(text == player2){
+        modal.innerText = "As always, YOU LOSED!";
+    }else{
+        modal.innerText = "The winner is " + text;
+    }
+
+    const retryButton = document.createElement("button");
+    retryButton.setAttribute("class", "retry");
+    retryButton.innerText = "Retry";
+    retryButton.addEventListener("click",()=>{
+        setupBoard();
+    });
+    modal.appendChild(retryButton);
+    APP.appendChild(modal);
+    gameover = true;
+}
+
+function drawTable(width, height){
+    const table = document.createElement("table");
+    let i = 0;
+    for(let y=0; y<height; y++){
+        const row = document.createElement("tr");
+        for(let x=0; x<width; x++){
+            const cell = document.createElement("td");
+            cell.setAttribute("id", i.toString());
+            row.appendChild(cell);
+            i++;
+        }
+        table.appendChild(row);
+    }
+    APP.appendChild(table);
+}
+
+function setupBoard(){
+    // Remove the modal and the table
+    try{APP.querySelector(".modal").remove();}catch(e){}
+    try{APP.querySelector("table").remove();}catch(e){}
+
+    gameover = false;
+    turn = player1;
+
+    // Brand new table
+    drawTable(3, 3);
+    originalBoard = Array.from(Array(9).keys());
+    cellsDOM = [...document.querySelectorAll("td")];
+    cellsDOM.forEach(cell => {
+        cell.addEventListener("click", e=>{
+            if(!isBotPlaying || (isBotPlaying && turn == player1)){
+                const click = handleClick(cell);
+                if(click) displayModal(click); // Means if there is a TIE or a winner
+
+                // Bot Turn After Each Click
+                if(isBotPlaying){
+                    const click = handleTurn(botTurn());
+                    if(click) displayModal(click); // Means if there is a TIE or a winner
+                }
+            }
+        });
+    });
+}
+
+function isCellEmpty(cell, board=originalBoard){
+    return typeof(board[cell]) == 'number';
+}
+
+function getEmptyCells(board=originalBoard){
+    return board.filter(cell => isCellEmpty(cell));
+}
+
+function TieGame(board=originalBoard){
+    for(let i=0; i<board.length; i++){
+        if(isCellEmpty(i, board)) return false;
+    }
+    return true;
+}
+
+function winner(board=originalBoard){
+    const winCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    let win = false;
+    winCombos.forEach(combo => {
+        const o = board[combo[0]];
+        if(board[combo[1]] == o && board[combo[2]] == o){
+            win = o;
+            return;
+        }
+    });
+    return win;
+}
+
+function handleClick(cell){
+    if(!gameover){
+        return handleTurn(parseInt(cell.id));
+    }else{
+        return false;
+    }
+}
+
+function handleTurn(cell){
+    if(gameover) return false;
+    if(isCellEmpty(cell)){
+        cellsDOM[cell].innerText = turn;
+        originalBoard[cell] = turn;
+        turn = (turn == player1) ? player2 : player1;
+
+        if(winner()) return winner();
+        if(TieGame()) return "TIE";
+
+        return false;
+    }
+}
+
+
+function botTurn(){
+    return getEmptyCells()[0];
+}
+
+
+setupBoard();
